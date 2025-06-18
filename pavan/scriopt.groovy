0063e76d7efd3c8ceecddf9974771db1ee55a93e
Pavan das <70870856+pavan931@users.noreply.github.com>
2025-06-18 15:28:05 +0530

    Update and rename file1.js to scriopt.groovy

---
+++ pavan/scriopt.groovy
@@ -0,0 +1,12 @@
+def buildApp(){
+   echo "Building an application"
+}
+
+def testApp(){
+  echo "Testing an application"
+}
+
+def deployApp(
+  echo "Deploying an application"
+  echo "Build Version is : ${params.VERSION}"
+  }
